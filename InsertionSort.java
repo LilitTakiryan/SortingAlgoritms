7cff23ab6265349cbc4d4830a69076208b7090fc
Lilit Takiryan <39703980+LilitTakiryan@users.noreply.github.com>
2021-10-22 21:42:02 +0400

    Update InsertionSort.java
    While loop will work until previous=0, because if we have a pair i.o in the middle of the array, we will need to check the previous pairs too, so each time we will have an iteration until we reach the beginning and continue iteration in for loop.

--- InsertionSort.java
+++ InsertionSort.java
@@ -4,13 +4,8 @@ class InsertionSort {
         for (int i = 1; i < arr.length; i++) {
             int cursor = arr[i];
             int prev = i - 1;
-
-            //while loop  will work until prev=0, then for loop will iterate once again,
-            //if second pair is done but first pair needs to be swapped -> prev>=0
             while (prev >= 0 && arr[prev] > cursor) {
                 arr[prev + 1] = arr[prev];
-                // we copy the array at index arr[prev]
-                //to the right side //prev+1
                 prev = prev - 1;
             }
             arr[prev + 1] = cursor;
